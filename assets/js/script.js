798a912e3bfa8ab5197d6eaabbcf410d3f899a61
nhodges <hodges27@outlook.com>
2022-02-11 17:50:06 -0500

    c statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,28 @@
-// Assignment code here
+// prompt user for password options
+function getPasswordOptions() {
+    // store length of password from user input
+    var length = parseInt(
+        prompt('How many characters would you like your password to contain?')
+    );
+
+    // conditional statement to check if password length is a number, prompt ends if false
+    if (Number.isNaN(length)) {
+        alert('Password length must be provided as a number');
+        return null;
+    }
+
+    // conditional statement to check if password length is at least 8 characters long, prompt ends of false
+    if (length < 8) {
+        alert('Password length must be at least 8 characters');
+        return null;
+    }
+
+    // conditional statement to check if password length is less than 128 characters long, prompt ends if false
+    if (length > 128) {
+        alert('Password length must be less than 129 characters');
+        return null;
+    }
+}
 
 
 // Get references to the #generate element
