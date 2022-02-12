4b32d4f42a98b499cc614ed8e8d09b72ed66efd1
nhodges <hodges27@outlook.com>
2022-02-11 21:16:03 -0500

    arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,104 @@
+// special characters array
+var specialCharacters = [
+    '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// numeric characters array
+var numericCharacters = [
+    '0', 
+    '1',
+    '2', 
+    '3', 
+    '4', 
+    '5', 
+    '6', 
+    '7', 
+    '8', 
+    '9'
+];
+
+// lowercase Characters array
+var hasLowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// uppercase Characters array
+var hasUpperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
 // prompt user for password options
 function getPasswordOptions() {
     // store length of password from user input
